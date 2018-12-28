8ca9c4e1f9aa3f3c47bbff1996512a5777d51fed
João Silva <joao.silva@r2discover.com>
2018-12-28 17:19:50 +0000

    Added headers

--- pkg/ser/ser.go
+++ pkg/ser/ser.go
@@ -1,4 +1,20 @@
-package ser
+/*
+Copyright 2018 EAA Toolbox Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ser /* import "r2discover.com/go/eaa-toolbox/pkg/ser" */
 
 import (
 	"bytes"
@@ -26,8 +42,6 @@ const (
 	BigEndian    Endianness = 0
 	LittleEndian Endianness = 1
 )
-
-//var FileSignature [14]byte = [14]byte{"L", "U", "C", "A", "M", "-", "R", "E", "C", "O", "R", "D", "E", "R"}
 
 var FileSignature [14]byte
 
